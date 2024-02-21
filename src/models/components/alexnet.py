21b3f09c6947265ce403705ce0722f9271ca8553
jmaczan <jedrzejpawel@maczan.pl>
2024-02-21 14:52:16 +0100

    Adapting AlexNet to handle MNIST dataset

--- src/models/components/alexnet.py
+++ src/models/components/alexnet.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -6,13 +7,13 @@ class AlexNet(nn.Module):
     Paper: https://proceedings.neurips.cc/paper_files/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
     """
 
-    def __init__(self):
+    def __init__(self, channels=3, first_fc_in_features=9216):
         super().__init__()
 
         self.model = nn.Sequential(
             # 1st conv layer
             nn.Conv2d(
-                in_channels=3,
+                in_channels=channels,
                 out_channels=96,
                 kernel_size=(11, 11),
                 stride=4,
@@ -37,7 +38,7 @@ class AlexNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=(3, 3), stride=2),
             # 1st fc layer with dropout
-            nn.Linear(in_features=9216, out_features=4096),
+            nn.Linear(in_features=first_fc_in_features, out_features=4096),
             nn.Dropout(p=0.5),
             nn.ReLU(),
             # 2nd fc layer with dropout
@@ -47,3 +48,11 @@ class AlexNet(nn.Module):
             # 3rd fc layer
             nn.Linear(in_features=4096, out_features=1000),
         )
+
+    def forward(self, x):
+        for i, layer in enumerate(self.model):
+            x = layer(x)
+            print(f"Layer {i}: {x.size()}")
+
+        x = torch.flatten(x, start_dim=1)
+        return x
